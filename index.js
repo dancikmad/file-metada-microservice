74228db97e36065102387185b228c43be3358e61
Daniel Crigan <crgdaniel@yahoo.com>
2024-11-09 20:02:33 +0000

    create POST request to receive: name, type, size

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer');
 require('dotenv').config()
 
 var app = express();
+let bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
+const upload = multer();
+
 
 app.use(cors());
+
+// Serve static files (if needed)
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
+// Endpoint to render the HTML page
+app.get('/', (req, res) => {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  // Check if a file was uploaded
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+
+  // Send back the file details as JSON
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
+});
 
 
 
